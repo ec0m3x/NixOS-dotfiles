0aaef4c0d30244a03bb6445d566b42b00757f3df
ecomex <skoch@sks-concept.de>
2024-08-10 15:21:47 +0200

    added smb automount

--- system/security/automount.nix
+++ system/security/automount.nix
@@ -5,4 +5,22 @@
   services.gvfs.enable = true;
   services.udisks2.enable = true;
   programs.gnome-disks.enable = true;
+
+  # Enable the systemd automount service
+
+  boot.supportedFilesystems = [ "cifs" ];
+
+  systemd.mounts = [{
+    description = "Mount for SMB Share";
+    what = "//192.168.0.30/ecomex-storage/";
+    where = "/home/ecomex/ecomex-storage";
+    type = "cifs";
+    options = "credentials=/home/ecomex/.smb/credentials-ecomex-storage,rw,uid=1000,gid=1000";
+    }];
+
+  systemd.automounts = [{
+    description = "Automount for SMB Share";
+    where = "/home/ecomex/ecomex-storage";
+    wantedBy = [ "multi-user.target" ];
+  }];
 }
